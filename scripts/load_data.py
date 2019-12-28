04c59fae4f2def4d7c0d3f5e16af3891bf2f60e1
shinya-yaginuma <yaginuuun@gmail.com>
2019-12-28 23:22:28 +0900

    特徴の追加、テストの時に行数を制限して実行時間を短くした

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -7,9 +7,9 @@ logger = get_logger()
 file_path = os.path.dirname(__file__)
 
 
-def read_data_all():
-    train = read_train()
-    test = read_test()
+def read_data_all(dev=False):
+    train = read_train(dev)
+    test = read_test(dev)
     # specs = read_specs()
     train_labels = read_train_labels()
     submission = read_submission()
@@ -17,14 +17,16 @@ def read_data_all():
     return train, test, train_labels, submission
 
 
-def read_train():
+def read_train(dev=False):
     logger.info('Reading train.csv')
-    return pd.read_csv(os.path.join(file_path, '../data/input/train.csv'))
+    N_ROW = None if not dev else 100000
+    return pd.read_csv(os.path.join(file_path, '../data/input/train.csv'), nrows=N_ROW)
 
 
-def read_test():
+def read_test(dev=False):
     logger.info('Reading test.csv')
-    return pd.read_csv(os.path.join(file_path, '../data/input/test.csv'))
+    N_ROW = None if not dev else 100000
+    return pd.read_csv(os.path.join(file_path, '../data/input/test.csv'), nrows=N_ROW)
 
 
 def read_specs():
