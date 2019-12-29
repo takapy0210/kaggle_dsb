56f480cd16932f33abeb49270c53b8a31bffabd0
shinya-yaginuma <yaginuuun@gmail.com>
2019-12-29 16:39:41 +0900

    一応trainingが動くまで

--- scripts/staging.py
+++ scripts/staging.py
@@ -5,11 +5,14 @@ from utils import get_logger
 logger = get_logger()
 
 
-def staging_train(train_labels: pd.DataFrame, features: pd.DataFrame) -> pd.DataFrame:
+def staging_train(train_labels: pd.DataFrame, features: pd.DataFrame) -> (pd.DataFrame, pd.Series):
     train_labels = train_labels.set_index('game_session').merge(features, how='left', left_index=True, right_index=True)
     train_labels = _drop_columns_train(train_labels)
+    X_train = train_labels.drop('accuracy_group', axis=1)
+    X_train.columns = ["".join(c if c.isalnum() else "_" for c in str(x)) for x in X_train.columns]  # カラム名にカンマなどが含まれており、lightgbmでエラーが出るため
+    y_train = train_labels['accuracy_group']
     logger.info(train_labels.head())
-    return train_labels
+    return X_train, y_train
 
 
 def staging_test(test: pd.DataFrame, features: pd.DataFrame) -> pd.DataFrame:
